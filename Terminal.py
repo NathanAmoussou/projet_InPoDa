a0355daab7b5f7d6da6353e0a317855a01ea5581
uvsqNathanAmoussou <nathan.amoussou@ens.uvsq.fr>
2022-12-04 17:46:12 +0100

    Ajout de fct jusqu'à top k users

---
+++ Terminal.py
@@ -0,0 +1,11 @@
+liste_commandes = {
+    
+}
+
+print("> InPoDa")
+print("> Taper 'exit' pour quitter.")
+print("> Taper 'help' pour afficher la liste des commandes.")
+while 1:
+    x = input("> ")
+    if x == "exit":
+        break
